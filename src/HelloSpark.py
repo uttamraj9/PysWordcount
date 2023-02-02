ece784c90c2c6ca45b60cadd3cccd111f3f701fa
uttamraj9 <uttamraj9@gmail.com>
2023-02-02 22:17:56 +0530

    WordCount_1

--- src/HelloSpark.py
+++ src/HelloSpark.py
@@ -5,4 +5,4 @@ spark = SparkSession.builder.master("local[10]").appName("WordCount").getOrCreat
 sc = spark.sparkContext
 words = sc.textFile('/tmp/uttam/sam_uttam.txt').flatMap(lambda line: line.split(' '))
 a = words.map(lambda word: (word, 1))
-a.collect()
+print(a.collect())
